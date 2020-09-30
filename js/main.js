e4ceb2c14bc7ecdb186f78ea7a001a64c6032ede
Luis Lopes <luis.lopes@protheus_srv.nk>
2020-09-30 17:19:05 -0300

    aula js

---
+++ js/main.js
@@ -0,0 +1,99 @@
+function clicou(){
+    document.getElementById('agradecimento').innerHTML = 'Obrigado por clicar';
+
+}
+
+/*
+function soma(n1, n2){
+    return n1+n2;
+}
+
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome)
+}
+
+var validar = 0;
+
+function validadeIdade(idade){
+    if(idade>=18){
+        validar = true;
+    }
+    else{
+        validar = false;
+    }
+    return validar;
+}
+
+var idade =prompt('Qual sua idade');
+validadeIdade(idade)
+console.log(validar);
+*/
+
+/*alert(soma(5,10));
+alert(setReplace('Vai Japão', 'Japão', 'Brasil'));*/
+
+/*
+var d =new Date();
+alert(d.getMinutes());
+alert(d.getHours());
+alert(d.getMinutes());
+*/
+
+/*var count;
+for(count = 0; count<=5; count++){
+    alert(count);
+}
+*/
+
+/*
+var count = 0;
+while(count <=5){
+    console.log(count);
+    alert(count);
+    count++;
+}
+*/
+
+/*
+var idade = prompt('Qual a sua idade?');
+if(idade>=18){
+    alert('maior de idade');
+}
+else{
+    alert('menor de idade');
+}
+*/
+
+/*var frutas = [{nome:'maça', cor:'vermelha'}, {nome:'uva', cor:'roxa'}];
+
+var fruta = {nome:'maça', cor:'vermelha'}
+console.log(fruta);
+console.log(fruta.nome);
+alert(frutas[1].nome);
+console.log(frutas);*/
+
+/*var lista = ['maça','pera','laranja'];
+console.log(lista[1]);
+lista.push('uva');
+lista.push('melancia');
+console.log(lista);
+lista.pop();
+console.log(lista);
+alert(lista[1]);
+
+console.log(lista.length);
+console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista.join(' - '));*/
+
+/*var nome = 'Luis Lopes';
+var n1 = 5;
+var n2 = 3;
+var frase = 'O Japão é o melhor time do mundo';
+//alert(nome+ ' tem ' + idade + ' anos');
+//alert(idade + idade2);
+console.log(nome);
+console.log(n1*n2);
+console.log(frase.toLowerCase());
+//alert(frase.replace("Japão", "Brasil"));
+console.log(frase.toLocaleUpperCase().replace('Japão', 'Brasil'));*/
